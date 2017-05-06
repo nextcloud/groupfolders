642833fe8c88b0dfc325e47f1f4f36bbc54921ca
Robin Appelman <robin@icewind.nl>
2017-05-06 15:19:06 +0200

    theme folder icon
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- js/files.js
+++ js/files.js
@@ -1,3 +1,8 @@
-$(function() {
-	OC.MimeType._mimeTypeIcons['dir-group'] = OC.imagePath('groupfolders', 'folder-group');
+$(function () {
+	if (OCA.Theming) {
+		// https://localcloud.icewind.me/apps/theming/img/groupfolders/folder-group.svg?v=34
+		OC.MimeType._mimeTypeIcons['dir-group'] = OC.generateUrl('/apps/theming/img/groupfolders/folder-group.svg?v=' + OCA.Theming.cacheBuster);
+	} else {
+		OC.MimeType._mimeTypeIcons['dir-group'] = OC.imagePath('groupfolders', 'folder-group');
+	}
 });
