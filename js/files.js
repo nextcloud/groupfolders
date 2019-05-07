719e96a41f921861c476870ea89458ab25a864af
Robin Appelman <robin@icewind.nl>
2019-05-07 18:36:48 +0200

    register folder icon only when the file app is being loaded

--- js/files.js
+++ js/files.js
@@ -1,8 +1,11 @@
 $(function () {
-	if (OCA.Theming) {
-		// https://localcloud.icewind.me/apps/theming/img/groupfolders/folder-group.svg?v=34
-		OC.MimeType._mimeTypeIcons['dir-group'] = OC.generateUrl('/apps/theming/img/groupfolders/folder-group.svg?v=' + OCA.Theming.cacheBuster);
-	} else {
-		OC.MimeType._mimeTypeIcons['dir-group'] = OC.imagePath('groupfolders', 'folder-group');
-	}
+	OC.Plugins.register('OCA.Files.App', {
+		attach: function() {
+			if (OCA.Theming) {
+				OC.MimeType._mimeTypeIcons['dir-group'] = OC.generateUrl('/apps/theming/img/groupfolders/folder-group.svg?v=' + OCA.Theming.cacheBuster);
+			} else {
+				OC.MimeType._mimeTypeIcons['dir-group'] = OC.imagePath('groupfolders', 'folder-group');
+			}
+		}
+	});
 });
