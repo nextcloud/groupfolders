e6703e460081d91a6ec5301841d5e79d2b35e001
Julius Härtl <jus@bitgrid.net>
2020-11-19 12:42:41 +0100

    Migrate acl column to be nullable for oracle compatibility
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

---
+++ lib/Migration/Version802000Date20201119112624.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\GroupFolders\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+class Version802000Date20201119112624 extends SimpleMigrationStep {
+
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options) {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+
+		$result = $this->ensureColumnIsNullable($schema, 'group_folders', 'acl');
+
+		return $result ? $schema : null;
+	}
+
+	protected function ensureColumnIsNullable(ISchemaWrapper $schema, string $tableName, string $columnName): bool {
+		$table = $schema->getTable($tableName);
+		$column = $table->getColumn($columnName);
+
+		if ($column->getNotnull()) {
+			$column->setNotnull(false);
+			return true;
+		}
+
+		return false;
+	}
+}
