73c624a2f8c9b57c6b42567fc64059d7bb59db40
Julius Härtl <jus@bitgrid.net>
2020-11-27 12:32:43 +0100

    Drop unneeded index if present
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- lib/Migration/Version802000Date20201119112624.php
+++ lib/Migration/Version802000Date20201119112624.php
@@ -1,6 +1,27 @@
 <?php
 
 declare(strict_types=1);
+/**
+ * @copyright Copyright (c) 2020 Julius Härtl <jus@bitgrid.net>
+ *
+ * @author Julius Härtl <jus@bitgrid.net>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 namespace OCA\GroupFolders\Migration;
 
@@ -16,6 +37,13 @@ class Version802000Date20201119112624 extends SimpleMigrationStep {
 		$schema = $schemaClosure();
 
 		$result = $this->ensureColumnIsNullable($schema, 'group_folders', 'acl');
+
+		// There might be a duplicate column group which was already indexed through being primary key in Version401001Date20190715092137
+		$table = $schema->getTable('group_folders_manage');
+		if ($table->hasIndex('groups_folder_manage_unique')) {
+			$table->dropIndex('groups_folder_manage_unique');
+			$result = true;
+		}
 
 		return $result ? $schema : null;
 	}
