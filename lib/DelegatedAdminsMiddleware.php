a040de35fc754cad59976235d82839e981f0b953
Cyrille Bollu <cyr.debian@bollu.be>
2021-04-30 15:09:29 +0200

    fixup: Proper tabulation in file DelegatedAdminsMiddleware.php (eventually!)
    Signed-off-by: Cyrille Bollu <cyr.debian@bollu.be>

--- lib/DelegatedAdminsMiddleware.php
+++ lib/DelegatedAdminsMiddleware.php
@@ -30,70 +30,70 @@ use Psr\Log\LoggerInterface;
 
 class DelegatedAdminsMiddleware extends Middleware {
 
-    /** @var ControllerMethodReflector */
-    private $reflector;
+	/** @var ControllerMethodReflector */
+	private $reflector;
 
-    /** @var DelegationService */
-    private $delegationService;
+	/** @var DelegationService */
+	private $delegationService;
 
-    /** @var LoggerInterface */
-    private $logger;
+	/** @var LoggerInterface */
+	private $logger;
 
-    /** @var IRequest */
-    private $request;
+	/** @var IRequest */
+	private $request;
 
-    /**
-     *
-     * @param ControllerMethodReflector $reflector
-     * @param DelegationService $delegationService
-     * @param IRequest $request
-     * @param LoggerInterface $logger
-     *
-     */
-    public function __construct(ControllerMethodReflector $reflector,
-                DelegationService $delegationService,
-                IRequest $request,
-                LoggerInterface $logger) {
+	/**
+	 *
+	 * @param ControllerMethodReflector $reflector
+	 * @param DelegationService $delegationService
+	 * @param IRequest $request
+	 * @param LoggerInterface $logger
+	 *
+	 */
+	public function __construct(ControllerMethodReflector $reflector,
+				DelegationService $delegationService,
+				IRequest $request,
+				LoggerInterface $logger) {
 
-                $this->reflector = $reflector;
-                $this->delegationService = $delegationService;
-                $this->logger = $logger;
-                $this->request = $request;
-    }
+				$this->reflector = $reflector;
+				$this->delegationService = $delegationService;
+				$this->logger = $logger;
+				$this->request = $request;
+	}
 
-    /**
-     *
-     * {@inheritDoc}
-     * @see \OCP\AppFramework\Middleware::beforeController()
-     *
-     * Throws an error when the user is not allowed to use the app's APIs
-     *
-     */
-    public function beforeController($controller, $methodName) {
+	/**
+	 *
+	 * {@inheritDoc}
+	 * @see \OCP\AppFramework\Middleware::beforeController()
+	 *
+	 * Throws an error when the user is not allowed to use the app's APIs
+	 *
+	 */
+	public function beforeController($controller, $methodName) {
 	if ($this->reflector->hasAnnotation('@RequireGroupFolderAdmin')) {	
-            if(!$this->delegationService->isAdmin()) {
-    	        $this->logger->error('User is not member of a delegated admins group');
-                throw new \Exception('User is not member of a delegated admins group', Http::STATUS_FORBIDDEN);
-            }
-        }
-    }
+			if(!$this->delegationService->isAdmin()) {
+				$this->logger->error('User is not member of a delegated admins group');
+				throw new \Exception('User is not member of a delegated admins group', Http::STATUS_FORBIDDEN);
+			}
+		}
+	}
 
-    /**
-     *
-     * {@inheritDoc}
-     * @see \OCP\AppFramework\Middleware::afterException()
-     *
-     */
-    public function afterException($controller, $methodName, \Exception $exception): Response {
-        if (stripos($this->request->getHeader('Accept'),'html') === false) {
-            $response = new JSONResponse(
-                ['message' => $exception->getMessage()],
-                (int)$exception->getCode()
-            );
-        } else {
-            $response = new TemplateResponse('core', '403', ['message' => $exception->getMessage()], 'guest');
-            $response->setStatus((int)$exception->getCode());
-        }
-        return $response;
-    }
+	/**
+	 *
+	 * {@inheritDoc}
+	 * @see \OCP\AppFramework\Middleware::afterException()
+	 *
+	 */
+	public function afterException($controller, $methodName, \Exception $exception): Response {
+		if (stripos($this->request->getHeader('Accept'),'html') === false) {
+			$response = new JSONResponse(
+				['message' => $exception->getMessage()],
+				(int)$exception->getCode()
+			);
+		} else {
+			$response = new TemplateResponse('core', '403', ['message' => $exception->getMessage()], 'guest');
+			$response->setStatus((int)$exception->getCode());
+		}
+		return $response;
+	}
 }
