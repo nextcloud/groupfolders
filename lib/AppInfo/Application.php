d5803f2e86fe66dd0b6e8575be582cc3380e11ba
Cyrille Bollu <cyr.debian@bollu.be>
2021-04-30 11:53:31 +0200

    fixup: spaces -> tabs
    Signed-off-by: Cyrille Bollu <cyr.debian@bollu.be>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -135,7 +135,7 @@ class Application extends App implements IBootstrap {
 		// Services and middleware needed to control accesses to the application and its API's
 		$context->registerService('DelegationService', function($c) {
 			return new DelegationService(
-	                       $c->query(IConfig::class),
+			       $c->query(IConfig::class),
 				$c->query(IGroupManager::class),
 				$c->query(IUserSession::class)
 			);
@@ -144,7 +144,7 @@ class Application extends App implements IBootstrap {
 			return new DelegatedAdminsMiddleware(
 				$c->query(DelegationService::class),
 				$c->query(IRequest::class),
-		        );
+			);
 		});
 		$context->registerMiddleware('OCA\GroupFolders\DelegatedAdminsMiddleware');
 	}
