022e0817bfb1edd181c96cfe16c2977b7bc7b923
Tim S <40753639+cetcondor@users.noreply.github.com>
2022-01-07 12:24:09 +0100

    Code did not use localisation string
    The group folders admin page always shows the English headline, no matter what UI language is chosen. The code simply had not yet used the localisation string.
    
    Signed-off-by: Tim Schrock <dev@schrock.net>

--- templates/index.php
+++ templates/index.php
@@ -1,6 +1,8 @@
 <?php script($_['appId'], ['groupfolders-settings']); ?>
 <div id="searchresults" style="display: none"></div>
 <div id="groupfolders-wrapper">
-	<h2>Group folders</h2>
+	<h2>
+		<?php p($l->t('Group folders')); ?>
+	</h2>
 	<div id="groupfolders-root"/>
 </div>
