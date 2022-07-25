7a537e12ce8b1262a58b577b826f42c524e6699e
Baptiste Fotia <fotia.baptiste@hotmail.com>
2022-07-25 16:51:54 +0200

    chore(TS, l10n): Translating of input descriptions
    Signed-off-by: Baptiste Fotia <fotia.baptiste@hotmail.com>

--- l10n/en_GB.js
+++ l10n/en_GB.js
@@ -14,6 +14,10 @@ OC.L10N.register(
     "Delete \"{folderName}\"?" : "Delete \"{folderName}\"?",
     "Folder name" : "Folder name",
     "Groups" : "Groups",
-    "Quota" : "Quota"
+    "Quota" : "Quota",
+    "Group folder admin delegation": "Group folder admin delegation",
+    "Nextcloud allows you to delegate the administration of groupfolders to non-admin users.": "Nextcloud allows you to delegate the administration of groupfolders to non-admin users.",
+    "Specify below the groups that will be allowed to manage groupfolders and use its API/REST. They will have access to all Groupfolders.": "Specify below the groups that will be allowed to manage groupfolders and use its API/REST. They will have access to all Groupfolders.",
+    "Specify below the groups that will be allowed to manage groupfolders and use its API/REST only. They will only have access to Groupfolders for which the have advanced permissions.": "Specify below the groups that will be allowed to manage groupfolders and use its API/REST only. They will only have access to Groupfolders for which the have advanced permissions."
 },
 "nplurals=2; plural=(n != 1);");
