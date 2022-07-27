94f72663ac4539803c09f3931c66ff3b47a43715
Baptiste Fotia <fotia.baptiste@hotmail.com>
2022-07-27 10:33:28 +0200

    chore(l10n, TS): Move 2 sentences
    Signed-off-by: Baptiste Fotia <fotia.baptiste@hotmail.com>

--- l10n/en_GB.js
+++ l10n/en_GB.js
@@ -17,8 +17,10 @@ OC.L10N.register(
     "Quota" : "Quota",
     "Group folder admin delegation": "Group folder admin delegation",
     "Nextcloud allows you to delegate the administration of groupfolders to non-admin users.": "Nextcloud allows you to delegate the administration of groupfolders to non-admin users.",
-    "Specify below the groups that will be allowed to manage groupfolders and use its API/REST. They will have access to all Groupfolders.": "Specify below the groups that will be allowed to manage groupfolders and use its API/REST. They will have access to all Groupfolders.",
-    "Specify below the groups that will be allowed to manage groupfolders and use its API/REST only. They will only have access to Groupfolders for which the have advanced permissions.": "Specify below the groups that will be allowed to manage groupfolders and use its API/REST only. They will only have access to Groupfolders for which the have advanced permissions.",
+    "Specify below the groups that will be allowed to manage groupfolders and use its API/REST.": "Specify below the groups that will be allowed to manage groupfolders and use its API/REST.",
+    "They will have access to all Groupfolders.": "They will have access to all Groupfolders.",
+    "Specify below the groups that will be allowed to manage groupfolders and use its API/REST only.": "Specify below the groups that will be allowed to manage groupfolders and use its API/REST only.",
+    "They will only have access to Groupfolders for which the have advanced permissions.": "They will only have access to Groupfolders for which the have advanced permissions.",
     "Add group": "Add group"
 },
 "nplurals=2; plural=(n != 1);");
