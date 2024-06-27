929f840ba4f2df7a62a6794b604be7081ad33148
skjnldsv <skjnldsv@protonmail.com>
2024-06-27 09:21:22 +0200

    feat: spread group folder expiry background job
    Signed-off-by: skjnldsv <skjnldsv@protonmail.com>

--- lib/BackgroundJob/ExpireGroupVersions.php
+++ lib/BackgroundJob/ExpireGroupVersions.php
@@ -24,15 +24,23 @@ declare(strict_types=1);
 
 namespace OCA\GroupFolders\BackgroundJob;
 
+use OCA\GroupFolders\AppInfo\Application;
+use OCA\GroupFolders\Folder\FolderManager;
 use OCA\GroupFolders\Versions\GroupVersionsExpireManager;
 use OCP\AppFramework\Utility\ITimeFactory;
 use OCP\BackgroundJob\TimedJob;
+use OCP\IAppConfig;
+use Psr\Log\LoggerInterface;
 
 class ExpireGroupVersions extends TimedJob {
-	private GroupVersionsExpireManager $expireManager;
-
-	public function __construct(GroupVersionsExpireManager $expireManager, ITimeFactory $timeFactory) {
-		parent::__construct($timeFactory);
+	public function __construct(
+		ITimeFactory $time,
+		private GroupVersionsExpireManager $expireManager,
+		private IAppConfig $appConfig,
+		private FolderManager $folderManager,
+		private LoggerInterface $logger,
+	) {
+		parent::__construct($time);
 
 		// Run once per hour
 		$this->setInterval(60 * 60);
@@ -42,7 +50,42 @@ class ExpireGroupVersions extends TimedJob {
 		$this->expireManager = $expireManager;
 	}
 
+	/**
+	 * Expiring groupfolder versions can be quite expensive.
+	 * We need to limit the amount of folders we expire per run.
+	 */
 	protected function run($argument) {
-		$this->expireManager->expireAll();
+		$lastFolder = $this->appConfig->getValueInt(Application::APP_ID, 'cron_last_folder_index', 0);
+		$folders = $this->folderManager->getAllFolders();
+
+		$folderCount = count($folders);
+		$currentRunHour = (int)date('G', $this->time->getTime());
+
+		// Calculate folders to process in the remaining hours, ensure at least one folder is processed
+		$toDo = max(1, (int)ceil(($folderCount - $lastFolder) / (24 - $currentRunHour)));
+
+		// If there are no folders, we don't need to do anything
+		if ($folderCount === 0) {
+			$this->logger->debug('No folders to expire', ['app' => 'cron']);
+			return;
+		}
+
+		// If we would go over the end of the list, wrap around
+		if ($lastFolder >= $folderCount) {
+			$lastFolder = 0;
+		}
+
+		// Save the updated folder index BEFORE processing the folders
+		$this->appConfig->setValueInt(Application::APP_ID, 'cron_last_folder_index', $lastFolder + $toDo);
+
+		// Determine the set of folders to process
+		$folderSet = array_slice($folders, $lastFolder, $toDo);
+		$folderIDs = array_map(function ($folder) {
+			return $folder['id'];
+		}, $folderSet);
+
+		// Log and start the expiration process
+		$this->logger->debug('Expiring versions for ' . count($folderSet) . ' folders', ['app' => 'cron', 'folders' => $folderIDs]);
+		$this->expireManager->expireFolders($folderSet);
 	}
 }
