37174bb12e59c8e567e9731c0595e7191702d63f
Louis Chemineau <louis@chmn.me>
2024-09-13 15:33:35 +0200

    =
    Signed-off-by: Louis Chemineau <louis@chmn.me>

--- cypress/e2e/files_versions/version_expiration.cy.ts
+++ cypress/e2e/files_versions/version_expiration.cy.ts
@@ -3,48 +3,25 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import type { User } from '@nextcloud/cypress'
-
-import { assertVersionContent, nameVersion, openVersionsPanel, uploadThreeVersions } from './filesVersionsUtils'
-import { addUserToGroup, createGroup, createGroupFolder, PERMISSION_DELETE, PERMISSION_READ, PERMISSION_WRITE } from '../groupfoldersUtils'
-import { navigateToFolder } from '../files/filesUtils'
+import { closeSidebar } from '../files/filesUtils'
+import { assertVersionContent, nameVersion, openVersionsPanel, setupFilesVersions } from './filesVersionsUtils'
 
 describe('Versions expiration', () => {
-	let randomGroupName: string
-	let randomGroupFolderName: string
-	let randomFileName: string
 	let randomFilePath: string
-	let user1: User
 
 	beforeEach(() => {
-		randomGroupName = Math.random().toString(36).replace(/[^a-z]+/g, '').substring(0, 10)
-		randomGroupFolderName = Math.random().toString(36).replace(/[^a-z]+/g, '').substring(0, 10)
-		randomFileName = Math.random().toString(36).replace(/[^a-z]+/g, '').substring(0, 10) + '.txt'
-		randomFilePath = `${randomGroupFolderName}/${randomFileName}`
-
-		cy.createRandomUser().then(_user => { user1 = _user })
-		createGroup(randomGroupName)
-
-		cy.then(() => {
-			addUserToGroup(randomGroupName, user1.userId)
-			createGroupFolder(randomGroupFolderName, randomGroupName, [PERMISSION_READ, PERMISSION_WRITE, PERMISSION_DELETE])
-
-			uploadThreeVersions(user1, randomFilePath)
-			cy.login(user1)
-		})
-
-		cy.visit('/apps/files')
-		navigateToFolder(randomGroupFolderName)
-		openVersionsPanel(randomFilePath)
-
+		setupFilesVersions()
+			.then(({ filePath }) => {
+				randomFilePath = filePath
+			})
 	})
 
 	it('Expire all versions', () => {
 		cy.runOccCommand('config:system:set versions_retention_obligation --value "0, 0"')
-		cy.runOccCommand('versions:expire')
+		cy.runOccCommand('groupfolders:expire')
 		cy.runOccCommand('config:system:set versions_retention_obligation --value auto')
-		cy.visit('/apps/files')
-		openVersionsPanel(randomFileName)
+		closeSidebar()
+		openVersionsPanel(randomFilePath)
 
 		cy.get('#tab-version_vue').within(() => {
 			cy.get('[data-files-versions-version]').should('have.length', 1)
@@ -58,10 +35,10 @@ describe('Versions expiration', () => {
 		nameVersion(2, 'v1')
 
 		cy.runOccCommand('config:system:set versions_retention_obligation --value "0, 0"')
-		cy.runOccCommand('versions:expire')
+		cy.runOccCommand('groupfolders:expire')
 		cy.runOccCommand('config:system:set versions_retention_obligation --value auto')
-		cy.visit('/apps/files')
-		openVersionsPanel(randomFileName)
+		closeSidebar()
+		openVersionsPanel(randomFilePath)
 
 		cy.get('#tab-version_vue').within(() => {
 			cy.get('[data-files-versions-version]').should('have.length', 2)
