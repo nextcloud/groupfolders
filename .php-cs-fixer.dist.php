d410d64003dbd382c2e70a963666ef9b543d4b57
provokateurin <kate@provokateurin.de>
2024-09-16 10:32:04 +0200

    chore(deps): Move cs-fixer, phpunit and psalm to vendor-bin
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
  * SPDX-FileCopyrightText: 2022 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
-require_once './vendor/autoload.php';
+require_once './vendor-bin/cs-fixer/vendor/autoload.php';
 
 use Nextcloud\CodingStandard\Config;
 
