2ae23e14aa8fb77ebf8c2368e46fc9d7f0ab0e91
provokateurin <kate@provokateurin.de>
2024-09-16 16:41:57 +0200

    fix: Enforce strict types everywhere
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- lib/Listeners/LoadAdditionalScriptsListener.php
+++ lib/Listeners/LoadAdditionalScriptsListener.php
@@ -1,11 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * SPDX-FileCopyrightText: 2020 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
-
-declare(strict_types=1);
 
 namespace OCA\GroupFolders\Listeners;
 
