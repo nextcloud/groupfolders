de45b727258addbeb621bf14c0c0ab7af9332677
provokateurin <kate@provokateurin.de>
2024-10-08 14:39:59 +0200

    refactor(FolderManager): Add default rootStorageId 0 for getFolder()
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- lib/Folder/FolderManager.php
+++ lib/Folder/FolderManager.php
@@ -247,7 +247,7 @@ class FolderManager {
 	 * @return ?array{id: mixed, mount_point: mixed, groups: array<string, array{displayName: string, type: string, permissions: integer}>, quota: int, size: int, acl: bool}
 	 * @throws Exception
 	 */
-	public function getFolder(int $id, int $rootStorageId): ?array {
+	public function getFolder(int $id, int $rootStorageId = 0): ?array {
 		$applicableMap = $this->getAllApplicable();
 
 		$query = $this->connection->getQueryBuilder();
