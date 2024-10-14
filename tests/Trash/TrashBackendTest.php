f7d1505ca02b42ef800df2e76638448a91a4314d
provokateurin <kate@provokateurin.de>
2024-10-14 17:51:14 +0200

    fix(Trash): Fix original location for deleting shared item
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- tests/Trash/TrashBackendTest.php
+++ tests/Trash/TrashBackendTest.php
@@ -19,9 +19,12 @@ use OCA\GroupFolders\ACL\UserMapping\UserMapping;
 use OCA\GroupFolders\Folder\FolderManager;
 use OCA\GroupFolders\Mount\GroupFolderStorage;
 use OCA\GroupFolders\Trash\TrashBackend;
+use OCP\Constants;
 use OCP\Files\Folder;
 use OCP\Files\IRootFolder;
 use OCP\IUser;
+use OCP\Server;
+use OCP\Share;
 use Test\TestCase;
 use Test\Traits\UserTrait;
 
@@ -212,5 +215,73 @@ class TrashBackendTest extends TestCase {
 		$this->assertCount(1, $this->trashBackend->listTrashRoot($this->normalUser));
 
 		$this->logout();
+	}
+
+	public function testWrongOriginalLocation(): void {
+		$shareManager = Server::get(Share\IManager::class);
+
+		$userA = $this->createUser('A', 'test');
+		$userAFolder = Server::get(IRootFolder::class)->getUserFolder('A');
+
+		$userB = $this->createUser('B', 'test');
+		$userBFolder = Server::get(IRootFolder::class)->getUserFolder('B');
+
+		$groupBackend = Server::get(Database::class);
+		$groupBackend->createGroup('C');
+		$groupBackend->addToGroup('A', 'C');
+		$groupBackend->addToGroup('B', 'C');
+		$this->assertCount(2, $groupBackend->usersInGroup('C'));
+
+		$groupFolderId = $this->folderManager->createFolder('D');
+		$this->folderManager->setFolderACL($groupFolderId, true);
+		$this->folderManager->addApplicableGroup($groupFolderId, 'C');
+		$this->folderManager->setGroupPermissions($groupFolderId, 'C', Constants::PERMISSION_ALL);
+		$this->assertInstanceOf(Folder::class, $userAFolder->get('D'));
+		$this->assertInstanceOf(Folder::class, $userBFolder->get('D'));
+
+		$this->loginAsUser('A');
+		$userAFolder->newFolder('D/E/F');
+		$userAFolder->newFile('D/E/F/G', 'foo');
+
+		$this->ruleManager->saveRule(new Rule(new UserMapping('group', 'C'), $userAFolder->get('D/E')->getId(), Constants::PERMISSION_READ, 0));
+		$this->ruleManager->saveRule(new Rule(new UserMapping('user', 'A'), $userAFolder->get('D/E')->getId(), Constants::PERMISSION_ALL, Constants::PERMISSION_READ | Constants::PERMISSION_UPDATE | Constants::PERMISSION_CREATE));
+		$this->ruleManager->saveRule(new Rule(new UserMapping('user', 'A'), $userAFolder->get('D/E/F')->getId(), Constants::PERMISSION_ALL, Constants::PERMISSION_ALL));
+
+		$folderShare = $shareManager->newShare();
+		$folderShare->setShareType(Share\IShare::TYPE_USER);
+		$folderShare->setSharedWith('B');
+		$folderShare->setSharedBy('A');
+		$folderShare->setPermissions(Constants::PERMISSION_ALL);
+		$folderShare->setNode($userAFolder->get('D/E/F'));
+		$folderShare = $shareManager->createShare($folderShare);
+		$this->assertNotEmpty($folderShare->getId());
+
+		$fileShare = $shareManager->newShare();
+		$fileShare->setShareType(Share\IShare::TYPE_USER);
+		$fileShare->setSharedWith('B');
+		$fileShare->setSharedBy('A');
+		$fileShare->setPermissions(19);
+		$fileShare->setNode($userAFolder->get('D/E/F/G'));
+		$fileShare = $shareManager->createShare($fileShare);
+		$this->assertNotEmpty($fileShare->getId());
+
+		$this->loginAsUser('B');
+		$this->assertTrue($userBFolder->get('F/G')->isDeletable());
+		$userBFolder->get('F/G')->delete();
+
+		$this->assertFalse($userAFolder->nodeExists('D/E/F/G'));
+
+		$trashedOfUserA = $this->trashBackend->listTrashRoot($userA);
+		$this->assertCount(1, $trashedOfUserA);
+		// Ensure original location inside share is correct
+		$this->assertSame('D/E/F/G', $trashedOfUserA[0]->getOriginalLocation());
+
+		$trashedOfUserB = $this->trashBackend->listTrashRoot($userB);
+		// Deleting share only unshares it, so no trash here
+		$this->assertCount(0, $trashedOfUserB);
+
+		// Restoring to original location works
+		$this->trashBackend->restoreItem($trashedOfUserA[0]);
+		$this->assertTrue($userAFolder->nodeExists('D/E/F/G'));
 	}
 }
