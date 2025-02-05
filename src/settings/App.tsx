70093b1a032e43d980f938e651888bc25e701557
Robin Appelman <robin@icewind.nl>
2025-02-05 17:17:32 +0100

    fix: show teams in acl manager settings
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- src/settings/App.tsx
+++ src/settings/App.tsx
@@ -368,7 +368,7 @@ export class App extends Component<{}, AppState> implements OC.Plugin<OC.Search.
 interface ManageAclSelectProps {
 	folder: Folder;
 	onChange: (type: string, id: string, manageAcl: boolean) => void;
-	onSearch: (name: string) => Promise<{ groups: AclManage[]; users: AclManage[]; }>;
+	onSearch: (name: string) => Promise<{ groups: AclManage[]; users: AclManage[]; circles: AclManage[] }>;
 }
 
 /**
@@ -382,13 +382,22 @@ function ManageAclSelect({ onChange, onSearch, folder }: ManageAclSelectProps) {
 	const handleSearch = (inputValue: string) => {
 		return new Promise<any>(resolve => {
 			onSearch(inputValue).then((result) => {
-				resolve([...result.groups, ...result.users])
+				resolve([...result.groups, ...result.users, ...result.circles])
 			})
 		})
 	}
 
-	const typeLabel = (item) => {
-		return item.type === 'user' ? t('groupfolders', 'User') : t('groupfolders', 'Group')
+	const typeLabel = (item: AclManage) => {
+		switch (item.type) {
+			case "circle":
+				return t('groupfolders', 'Team');
+			case "group":
+				return t('groupfolders', 'Group');
+			case "user":
+				return t('groupfolders', 'User');
+			default:
+				return t('groupfolders', 'Unknown');
+		}
 	}
 
 	return <AsyncSelect
