f94c51bfa2cbf131ddb97d2cbcb9ad279f014839
Robin Appelman <robin@icewind.nl>
2025-09-27 16:41:40 +0200

    chore: do the rootid/storageid migration again
    Signed-off-by: Robin Appelman <robin@icewind.nl>

---
+++ lib/Migration/Version2000002Date20250927142943.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\GroupFolders\Migration;
+
+/**
+ * Version20001Date20250612140256, but with a higher version
+ */
+class Version2000002Date20250927142943 extends Version20001Date20250612140256 {
+}
