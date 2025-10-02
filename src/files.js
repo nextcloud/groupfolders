3d5ee80267fd30776ced9664ffcd7a025d2c6b77
Ferdinand Thiessen <opensource@fthiessen.de>
2025-10-03 00:31:08 +0200

    chore: adjust leftover files for new ELint config
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- src/files.js
+++ src/files.js
@@ -2,10 +2,10 @@
  * SPDX-FileCopyrightText: 2018 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
+
 import { generateUrl, imagePath } from '@nextcloud/router'
 import { initFilesClient } from './client.js'
 
-// eslint-disable-next-line
 __webpack_nonce__ = btoa(OC.requestToken)
 // eslint-disable-next-line
 __webpack_public_path__ = OC.linkTo('groupfolders', 'js/')
