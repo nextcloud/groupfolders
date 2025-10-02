278e213e5e2bc4fe70ecfb36507cbd3e4ee65a6f
Ferdinand Thiessen <opensource@fthiessen.de>
2025-10-03 00:06:29 +0200

    refactor: adjust cypress tests to now enforced code style
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- cypress/e2e/files_versions/version_creation.cy.ts
+++ cypress/e2e/files_versions/version_creation.cy.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import { setupFilesVersions } from './filesVersionsUtils'
+import { setupFilesVersions } from './filesVersionsUtils.ts'
 
 describe('Versions creation', () => {
 	beforeEach(() => {
