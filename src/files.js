df2ee9c3f2bd3d2bf41853f8ce4fada09090df50
provokateurin <kate@provokateurin.de>
2025-10-14 08:11:59 +0200

    fix: Fix sidebar not loading
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- src/files.js
+++ src/files.js
@@ -2,7 +2,6 @@
  * SPDX-FileCopyrightText: 2018 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
-import { generateUrl, imagePath } from '@nextcloud/router'
 import { initFilesClient } from './client.js'
 
 // eslint-disable-next-line
@@ -11,12 +10,6 @@ __webpack_nonce__ = btoa(OC.requestToken)
 __webpack_public_path__ = OC.linkTo('groupfolders', 'js/')
 
 window.addEventListener('DOMContentLoaded', () => {
-	if (OCA.Theming) {
-		OC.MimeType._mimeTypeIcons['dir-group'] = generateUrl('/apps/theming/img/groupfolders/folder-group.svg?v=' + OCA.Theming.cacheBuster)
-	} else {
-		OC.MimeType._mimeTypeIcons['dir-group'] = imagePath('groupfolders', 'folder-group')
-	}
-
 	if (!OCA?.Sharing?.ShareTabSections) {
 		return
 	}
