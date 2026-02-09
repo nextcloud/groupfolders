4402859aa864db7a9be9461a32af8d4c6c2ed8a0
provokateurin <kate@provokateurin.de>
2026-02-09 13:35:52 +0100

    fix(ACL): Improve rule checking
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- lib/ACL/ACLStorageWrapper.php
+++ lib/ACL/ACLStorageWrapper.php
@@ -100,7 +100,7 @@ class ACLStorageWrapper extends Wrapper {
 		}
 		return  ($sourceParent === $targetParent ||
 			$this->checkPermissions($sourceParent, Constants::PERMISSION_DELETE)) &&
-			$this->checkPermissions($source, Constants::PERMISSION_UPDATE & Constants::PERMISSION_READ) &&
+			$this->checkPermissions($source, Constants::PERMISSION_UPDATE | Constants::PERMISSION_READ) &&
 			$this->checkPermissions($target, $permissions) &&
 			parent::rename($source, $target);
 	}
