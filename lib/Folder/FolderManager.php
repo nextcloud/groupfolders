36b97bda11c34fce3cb51bbb57c3778133544bac
Kevin <76160206+kevin-256@users.noreply.github.com>
2026-02-16 15:52:50 +0100

    Enhance the removeFolder function to also delete related folder entries from other tables
    Added deletion of folder entries from the `oc_group_folders_manage` and `oc_group_folders_trash` tables during folder removal. Also reordered the deletion sequence in case foreign key constraints are added to the database in the future.
    
    Signed-off-by: Kevin <76160206+kevin-256@users.noreply.github.com>

--- lib/Folder/FolderManager.php
+++ lib/Folder/FolderManager.php
@@ -883,9 +883,12 @@ class FolderManager {
 	 */
 	public function removeFolder(int $folderId): void {
 		$query = $this->connection->getQueryBuilder();
-
-		$query->delete('group_folders')
-			->where($query->expr()->eq('folder_id', $query->createNamedParameter($folderId, IQueryBuilder::PARAM_INT)));
+		$query->delete('group_folders_manage')
+			->where(
+				$query->expr()->eq(
+					'folder_id', $query->createNamedParameter($folderId, IQueryBuilder::PARAM_INT),
+				),
+			);
 		$query->executeStatement();
 		
 		$query = $this->connection->getQueryBuilder();
@@ -895,6 +898,20 @@ class FolderManager {
 					'folder_id', $query->createNamedParameter($folderId, IQueryBuilder::PARAM_INT),
 				),
 			);
+		$query->executeStatement();
+
+		$query = $this->connection->getQueryBuilder();
+		$query->delete('group_folders_trash')
+			->where(
+				$query->expr()->eq(
+					'folder_id', $query->createNamedParameter($folderId, IQueryBuilder::PARAM_INT),
+				),
+			);
+		$query->executeStatement();
+
+		$query = $this->connection->getQueryBuilder();
+		$query->delete('group_folders')
+			->where($query->expr()->eq('folder_id', $query->createNamedParameter($folderId, IQueryBuilder::PARAM_INT)));
 		$query->executeStatement();
 		
 		$this->eventDispatcher->dispatchTyped(new CriticalActionPerformedEvent('The groupfolder with id %d was removed', [$folderId]));
