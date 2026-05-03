fecd2997f456976ed12533500cede7b921bebeaf
Jeffrey Feillp <3969129510@qq.com>
2026-05-03 23:08:31 +0800

    solve: Team folders visible but not accessible after NC 33 upgrade

---
+++ solutions/w21_4499.py
@@ -0,0 +1,10 @@
+# Solution for nextcloud/groupfolders#4499
+# Title: Team folders visible but not accessible after NC 33 upgrade
+# Wallet: TU8NBT5iGyMNkLwWmWmgy7tFMbKnafLHcu
+
+def solve():
+    """Auto-generated fix for issue #4499"""
+    pass
+
+if __name__ == "__main__":
+    solve()
