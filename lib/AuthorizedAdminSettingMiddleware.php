6308415e4380c0672536660831cdf53361a11fe1
provokateurin <kate@provokateurin.de>
2026-06-03 14:58:34 +0200

    fix(Delegation): Fix checks
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- lib/AuthorizedAdminSettingMiddleware.php
+++ lib/AuthorizedAdminSettingMiddleware.php
@@ -8,7 +8,9 @@
 namespace OCA\GroupFolders;
 
 use Exception;
+use OC\AppFramework\Middleware\Security\Exceptions\NotAdminException;
 use OCA\GroupFolders\Attribute\RequireGroupFolderAdmin;
+use OCA\GroupFolders\Folder\FolderManager;
 use OCA\GroupFolders\Service\DelegationService;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http;
@@ -17,12 +19,15 @@ use OCP\AppFramework\Http\Response;
 use OCP\AppFramework\Http\TemplateResponse;
 use OCP\AppFramework\Middleware;
 use OCP\IRequest;
+use OCP\IUserSession;
 use ReflectionMethod;
 
 class AuthorizedAdminSettingMiddleware extends Middleware {
 	public function __construct(
 		private DelegationService $delegatedService,
 		private IRequest $request,
+		private IUserSession $userSession,
+		private FolderManager $folderManager,
 	) {
 	}
 
@@ -31,9 +36,24 @@ class AuthorizedAdminSettingMiddleware extends Middleware {
 	 */
 	public function beforeController(Controller $controller, string $methodName): void {
 		$method = new ReflectionMethod($controller, $methodName);
-		if ($method->getAttributes(RequireGroupFolderAdmin::class) !== [] && !$this->delegatedService->hasApiAccess()) {
-			throw new Exception('Logged in user must be an admin, a sub admin or gotten special right to access this setting');
+		if ($method->getAttributes(RequireGroupFolderAdmin::class) === []) {
+			return;
 		}
+
+		if ($this->delegatedService->isAdminNextcloud() || $this->delegatedService->isDelegatedAdmin()) {
+			return;
+		}
+
+		if ($this->delegatedService->hasOnlyApiAccess()
+			&& ($user = $this->userSession->getUser()) !== null
+			&& ($id = $this->request->getParam('id')) !== null) {
+			/** @var string $id */
+			if ($this->folderManager->canManageACL((int)$id, $user)) {
+				return;
+			}
+		}
+
+		throw new NotAdminException('Logged in user must be an admin, a sub admin or gotten special right to access this setting');
 	}
 
 	public function afterException(Controller $controller, string $methodName, Exception $exception): Response {
