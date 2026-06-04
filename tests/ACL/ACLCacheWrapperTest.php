36fd2f8f6286e54e93e94ae8ef48e0f8cd029d1b
Robin Appelman <robin@icewind.nl>
2026-06-04 19:44:21 +0200

    test: add test for searching with ACL filtered results
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- tests/ACL/ACLCacheWrapperTest.php
+++ tests/ACL/ACLCacheWrapperTest.php
@@ -9,28 +9,77 @@ declare(strict_types=1);
 namespace OCA\groupfolders\tests\ACL;
 
 use OC\Files\Cache\CacheEntry;
+use OC\Files\Search\SearchComparison;
+use OC\Files\Search\SearchOrder;
+use OC\Files\Search\SearchQuery;
+use OC\Files\Storage\Temporary;
 use OCA\GroupFolders\ACL\ACLCacheWrapper;
 use OCA\GroupFolders\ACL\ACLManager;
+use OCA\GroupFolders\ACL\Rule;
+use OCA\GroupFolders\ACL\RuleManager;
+use OCA\GroupFolders\ACL\UserMapping\IUserMapping;
+use OCA\GroupFolders\ACL\UserMapping\IUserMappingManager;
+use OCA\GroupFolders\Trash\TrashManager;
 use OCP\Constants;
 use OCP\Files\Cache\ICache;
+use OCP\Files\Cache\ICacheEntry;
+use OCP\Files\Search\ISearchComparison;
+use OCP\Files\Search\ISearchOrder;
+use OCP\IUser;
 use PHPUnit\Framework\MockObject\MockObject;
+use Psr\Log\LoggerInterface;
 use Test\TestCase;
 
 /**
  * @group DB
  */
 class ACLCacheWrapperTest extends TestCase {
-	private ACLManager&MockObject $aclManager;
+	private ACLManager $aclManager;
 	private ICache&MockObject $source;
 	private ACLCacheWrapper $cache;
 	private array $aclPermissions = [];
 
+	private function makeRule(int $permission): Rule {
+		return new Rule($this->createMock(IUserMapping::class), 0, Constants::PERMISSION_ALL, $permission);
+	}
+
+	/**
+	 * @param string[] $paths
+	 * @return array<string, Rule[]>
+	 */
+	private function getRulesForPaths(IUser $user, int $storageId, array $paths): array {
+		return array_combine($paths, array_map(
+			fn (string $path) => isset($this->aclPermissions[$path]) ? [$this->makeRule($this->aclPermissions[$path])] : [],
+			$paths
+		));
+	}
+
 	protected function setUp(): void {
 		parent::setUp();
 
-		$this->aclManager = $this->createMock(ACLManager::class);
-		$this->aclManager->method('getACLPermissionsForPath')
-			->willReturnCallback(fn (int $_storageId, string $path) => $this->aclPermissions[$path] ?? Constants::PERMISSION_ALL);
+		$user = $this->createMock(IUser::class);
+		$ruleManager = $this->createMock(RuleManager::class);
+		$ruleManager->method('getRulesForFilesByPath')
+			->willReturnCallback($this->getRulesForPaths(...));
+		$ruleManager->method('getRulesForPrefix')
+			->willReturnCallback(fn (IUser $user, int $storageId, string $prefix) => array_map(
+				fn (int $permission) => [$this->makeRule($permission)],
+				array_filter(
+					$this->aclPermissions,
+					fn (string $path) => str_starts_with($path, $prefix),
+					ARRAY_FILTER_USE_KEY,
+				))
+			);
+		$mappingManager = $this->createMock(IUserMappingManager::class);
+
+		$this->aclManager = new ACLManager(
+			$ruleManager,
+			$this->createMock(TrashManager::class),
+			$mappingManager,
+			$this->createMock(LoggerInterface::class),
+			$user
+		);
+
 		$this->source = $this->createMock(ICache::class);
 		$this->source->method('getNumericStorageId')
 			->willReturn(1);
@@ -73,5 +122,35 @@ class ACLCacheWrapperTest extends TestCase {
 		];
 
 		$this->assertEquals($expected, $result);
+	}
+
+	public function testSearchNonRead(): void {
+		$sourceStorage = new Temporary([]);
+		$sourceStorage->mkdir('foo');
+		$sourceStorage->touch('foo/test1.txt', 100);
+		$sourceStorage->touch('foo/test2.txt', 101);
+		$sourceStorage->touch('foo/test3.txt', 102);
+		$sourceStorage->mkdir('bar');
+		$sourceStorage->touch('bar/test1.txt', 200);
+		$sourceStorage->touch('bar/test2.txt', 201);
+		$sourceStorage->touch('bar/test3.txt', 202);
+		$sourceStorage->getScanner()->scan('');
+		$this->aclPermissions['bar'] = 0;
+
+		$cache = new ACLCacheWrapper($sourceStorage->getCache(), $this->aclManager, false);
+
+		$query = new SearchQuery(
+			new SearchComparison(ISearchComparison::COMPARE_LIKE, 'name', '%test%'),
+			2,
+			0,
+			[new SearchOrder(ISearchOrder::DIRECTION_DESCENDING, 'mtime')]
+		);
+		$result = $cache->searchQuery($query);
+		$paths = array_map(fn (ICacheEntry $entry) => $entry->getPath(), $result);
+
+		$this->assertEquals([
+			'foo/test3.txt',
+			'foo/test2.txt'
+		], $paths);
 	}
 }
