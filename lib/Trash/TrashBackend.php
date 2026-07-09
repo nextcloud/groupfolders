ff9a933a939e0f9dfd2c8314f4f755593cfb8a21
Carl Schwan <carlschwan@kde.org>
2026-07-09 11:17:15 +0200

    perf: Only preload acl rules if folder has acl enabled
    Signed-off-by: Carl Schwan <carlschwan@kde.org>

--- lib/Trash/TrashBackend.php
+++ lib/Trash/TrashBackend.php
@@ -483,7 +483,6 @@ class TrashBackend implements ITrashBackend {
 			$trashFolder = $this->setupTrashFolder($folder);
 			$content = $trashFolder->getDirectoryListing();
 			$userCanManageAcl = $this->folderManager->canManageACL($folder->id, $user, true);
-			$this->aclManagerFactory->getACLManager($user)->preloadRulesForFolder($folder->storageId, $trashFolder->getId());
 
 			$itemsForFolder = array_map(function (Node $item) use ($user, $folder, $indexedRows) {
 				$pathParts = pathinfo($item->getName());
@@ -511,7 +510,9 @@ class TrashBackend implements ITrashBackend {
 
 			// perform per-item ACL checks if the user doesn't have manage permissions
 			if ($folder->acl && !$userCanManageAcl) {
-				$itemsForFolder = array_filter($itemsForFolder, function (GroupTrashItem $item) use ($itemsByOriginalLocation) {
+				$this->aclManagerFactory->getACLManager($user)->preloadRulesForFolder($folder->storageId, $trashFolder->getId());
+
+				$itemsForFolder = array_filter($itemsForFolder, function (GroupTrashItem $item) use ($itemsByOriginalLocation): bool {
 					// if we for any reason lost track of the original location, hide the item for non-managers as a fail-safe
 					if ($item->getInternalOriginalLocation() === '') {
 						return false;
